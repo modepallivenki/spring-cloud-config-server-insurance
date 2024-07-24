88b41dbe94a46a6d9d30ebdf8b8ef0fcc8e44e06
modepallivenki <modepallivenki@gmail.com>
2024-07-24 15:26:51 +0200

    first commit

---
+++ src/main/java/com/tech/cloud/config/server/insurance/SpringCloudConfigServerInsuranceApplication.java
@@ -0,0 +1,15 @@
+package com.tech.cloud.config.server.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerInsuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/cloud/config/server/insurance/SpringCloudConfigServerInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.cloud.config.server.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
